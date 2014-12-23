6db1753a6e9dc6448c014fbca30ba2486df08e41
jbheron <jessbheron@gmail.com>
2014-12-23 15:20:18 -0800

    Refactor to ABCheck and ArithGeo

--- ABCheck.rb
+++ ABCheck.rb
@@ -1,35 +1,29 @@
-# Using the Ruby language, have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false. 
+# Using the Ruby language, have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false.
 
 def ABCheck(str)
-  answer ||= false
-  a = str.split(//)
-  for i in 0...a.length do
-    if a[i] == "a"
-      if a[i + 4] == "b"
-        answer = true
-      end
-    end
+  str.split(//).each_with_index do |char, i|
+    return true if char == "a" && str[i + 4] == "b"
   end
-  return answer
+  false
 end
 
 
 
-ABCheck("lane borrowed")
-ABCheck("hckieiei")
+p ABCheck("lane borrowed")
+p ABCheck("hckieiei")
 
 
 # def ABCheck(str)
-#   check = str.split(//)
-#   for i in 0...str.length
-#     puts check[i]
-#     if check[i] == "a" and check[i + 4] == "b"
-#       return true
-#     end
-#   end
-#   return false
+  # answer ||= false
+  # a = str.split(//)
+  # for i in 0...a.length do
+    # if a[i] == "a"
+      # if a[i + 4] == "b"
+        # answer = true
+      # end
+    # end
+  # end
+  # return answer
 # end
+#
 
-
-  
-        
